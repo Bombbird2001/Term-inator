c74764c51851bf34b3d57993b04850d97de2f776
Sasmik23 <36692672+Sasmik23@users.noreply.github.com>
2018-11-14 01:13:48 -0500

    UI display update

--- Term-inator/popup.js
+++ Term-inator/popup.js
@@ -1,20 +1,54 @@
+var parsedTcArray;
+
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		if (request.type == "sendUI") {
 			console.log("Received!");
-			var parsedTc = request.data;
-			console.log("tclink: " + parsedTc);
+		  var parsedTc = request.data;
+      parsedTcArray = parsedTc.split("#");
+      removeWeirdElements();
+      importantFunction();
 		}
 	});
 
-$(".btn.important").click(function(){
+document.getElementById("important").addEventListener("click", importantFunction);
 
-});
+if (parsedTcArray == undefined) {
+  document.getElementById("tc").innerHTML = "Loading...";
+}
 
-$(".btn.paste").click(function(){
+function removeWeirdElements() {
+  let newArray = [];
+  for (let i = 0; i < parsedTcArray.length; i++) {
+    if (parsedTcArray[i].length >= 10) {
+      newArray.push(parsedTcArray[i]);
+    }
+  }
+  parsedTcArray = newArray;
+}
 
-});
+function importantFunction() {
+  document.getElementById("tc").innerHTML = "";
+  // Make a container element for the list
+     var listContainer = document.getElementById('tc');
 
-$(".btn.others").click(function(){
+     // Make the list
+     var listElement = document.createElement('ul');
 
-});
+     // Add it to the page
+     listContainer.appendChild(listElement);
+
+     // Set up a loop that goes through the items in listItems one at a time
+     var numberOfListItems = parsedTcArray.length;
+
+     for (var i = 0; i < numberOfListItems; ++i) {
+         // create an item for each one
+         var listItem = document.createElement('li');
+
+         // Add the item text
+         listItem.innerHTML = parsedTcArray[i];
+
+         // Add listItem to the listElement
+         listElement.appendChild(listItem);
+       }
+}
